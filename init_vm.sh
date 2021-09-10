e88961bc102dd3e691011c4eef44069e19c07c32
racytech <racytech0x0@gmail.com>
2021-09-10 17:08:06 -0400

    installation cfgs

---
+++ init_vm.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+./vm_init/install_nginx
+./vm_init/install_go
--- vm_init/install_nginx.sh
+++ vm_init/install_nginx.sh
@@ -1,4 +1,11 @@
 #!/bin/sh
 
-sudo apt update && sudo upt install nginx
+sudo apt update && sudo upt install nginx --with-stream
 sudo ufw allow 'Nginx HTTP'
+
+sudo cp ./vm_init/tcp_host.conf /etc/nginx/sites-enabled/
+
+sudo nginx -t
+# TODO error check
+
+sudo systemctl reload nginx
