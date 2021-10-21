6622f3f55c7fe3db4cb844ec8e1b5044308c1b63
racytech <racytech0x0@gmail.com>
2021-10-20 20:41:59 -0400

    run_rpctest_replay.sh update

--- run_rpctest_replay.sh
+++ run_rpctest_replay.sh
@@ -1,25 +1,37 @@
 #!/bin/sh
 
 # this script allows to run rpc tests in the background process
-# the goal is to keep erigon running after terminal window is closed
+# the goal:
+#       to keep erigon running after terminal window is closed
+#       testing without running a server
 
-echo "Please enter Erigon branch you wish to test (default 'devel'):"
-read ERIGON_BRANCH
+echo "This command will restart Erigon and RPCdaemon. Do you wish to continue? [y/n] (default 'y')"
+read ANSWER
 
-if [ -z "$ERIGON_BRANCH" ]; then
-    ERIGON_BRANCH="devel"
+if [ -z "$ANSWER" ] || [ "$ANSWER" = "y" ] || [ "$ANSWER" = "yes" ]; then
+    echo "Please enter Erigon branch you wish to test (default 'devel'):"
+    read ERIGON_BRANCH
+
+    if [ -z "$ERIGON_BRANCH" ]; then
+        ERIGON_BRANCH="devel"
+    fi
+    echo "Erigon branch is set to: $ERIGON_BRANCH"
+
+    echo ""
+
+    echo "Please enter the rcptest branch (default 'main'):"
+    read TEST_BRANCH
+    if [ -z "$TEST_BRANCH" ]; then
+        TEST_BRANCH="main"
+    fi
+    echo "Rpctest branch is set to: $TEST_BRANCH"
+
+    OUT_FILE=rpctest_replay.out
+    echo "Starting rpctest_replay.sh script in background process... see $OUT_FILE for output"
+    nohup ./qa_scripts/rpctest_replay.sh $ERIGON_BRANCH $TEST_BRANCH >$OUT_FILE 2>&1 &
+elif [ "$ANSWER" = "n" ] || [ "$ANSWER" = "no" ]; then
+    exit 0
+else
+    echo "Unknown input: $ANSWER"
+    exit 1
 fi
-echo "Erigon branch is set to: $ERIGON_BRANCH"
-
-echo ""
-
-echo "Please enter the rcptest branch (default 'main'):"
-read TEST_BRANCH
-if [ -z "$TEST_BRANCH" ]; then
-    TEST_BRANCH="main"
-fi
-echo "Rpctest branch is set to: $TEST_BRANCH"
-
-OUT_FILE=rpctest_replay.out
-echo "Starting rpctest_replay.sh script in background process... see $OUT_FILE for output"
-nohup ./qa_scripts/rpctest_replay.sh $ERIGON_BRANCH $TEST_BRANCH >$OUT_FILE 2>&1 &
