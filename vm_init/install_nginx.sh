886d97b9e16fd030586d0fca6945d8f7a277ae27
racytech <racytech0x0@gmail.com>
2021-10-22 23:16:07 -0400

    removed install_nginx.sh

--- vm_init/install_nginx.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-# VERSION=1.20.1
-
-# wget https://nginx.org/download/nginx-$VERSION.tar.gz
-# tar zxf nginx-$VERSION.tar.gz
-# cd nginx-$VERSION
-
-# ./configure --sbin-path=/usr/local/sbin --with-stream
