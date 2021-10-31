c0f4aeb5357a0cfff33bb82d0fa6242fa87f8cb1
racytech <racytech0x0@gmail.com>
2021-10-31 18:28:29 -0400

    jenkinx nginx setup

--- vm_init/copy_nginx_conf.sh
+++ vm_init/copy_nginx_conf.sh
@@ -1,12 +1,13 @@
 #!/bin/sh
 
 CUR_DIR=$(pwd)
-sudo cp $CUR_DIR/web/web.conf /etc/nginx/sites-enabled/
+# sudo cp $CUR_DIR/web/web.conf /etc/nginx/sites-enabled/
 
-sudo mkdir -p /etc/nginx/stream-enabled/
-sudo cp $CUR_DIR/web/tcp.conf /etc/nginx/stream-enabled/
+# sudo mkdir -p /etc/nginx/stream-enabled/
+# sudo cp $CUR_DIR/web/tcp.conf /etc/nginx/stream-enabled/
 
-sudo rm /etc/nginx/sites-enabled/default
+# sudo rm /etc/nginx/sites-enabled/default
 
+sudo cp $CUR_DIR/web/jenkins.conf /etc/nginx/sites-enabled/
 sudo nginx -t
 sudo systemctl reload nginx
