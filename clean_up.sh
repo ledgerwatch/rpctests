081f5e7a4be500c86744c11288e06be0bc593bed
racytech <racytech0x0@gmail.com>
2021-11-04 20:21:50 -0400

    clean_up error fix

--- clean_up.sh
+++ clean_up.sh
@@ -18,7 +18,7 @@ done
 LAST_BUILD_DIR="$LOGS_DIR/$BUILD_ID"
 
 echo "Removing all directories in $LOGS_DIR except last build=$BUILD_ID"
-for dir in "$LOGS_DIR/*"; do 
+for dir in $LOGS_DIR/*; do 
 
     if [ ! $dir = $LAST_BUILD_DIR ]; then 
         echo "Removing directory $dir..."
